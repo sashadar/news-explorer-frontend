5c6c6bb9d0e9fc16b30c4190f7bc55cc9b474bc7
Alex D <alex.darincev@gmail.com>
2022-07-21 11:48:48 +0300

    (header): set custom borders and pointers for navigation links and buttons

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -3,14 +3,14 @@ import { Link } from 'react-router-dom';
 
 function Navigation() {
   return (
-    <nav>
+    <nav className='navigation'>
       <ul className='navigation__link-list'>
-        <li className='navigation_link-list-item'>
+        <li className='navigation__link-list-item navigation__link-list-item_active'>
           <Link to='/' className='navigation__link'>
             Home
           </Link>
         </li>
-        <li className='navigation_link-list-item'>
+        <li className='navigation__link-list-item navigation__link-list-item_articles navigation__link-list-item_active'>
           <Link to='/saved-news' className='navigation__link'>
             Saved articles
           </Link>
