8829367b52d0fe2b3efe92390186a989c037f765
Alex D <alex.darincev@gmail.com>
2022-07-30 21:59:49 +0300

    (Header/Navigation): moved buttons from header to navigation component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,17 +8,7 @@ import logoutIconDark from '../../images/icons/button-logout-icon-dark.svg';
 function Header() {
   return (
     <header className='header'>
-      <Link to='/' className='header__title'>
-        NewsExplorer
-      </Link>
-      <div className='header__container'>
-        <Navigation></Navigation>
-        {/* <button className='header__button'>Sign In</button> */}
-        <button className='header__button header__button_logout'>
-          Elise{' '}
-          <img className='header__logout-icon' alt='logout' src={logoutIcon} />
-        </button>
-      </div>
+      <Navigation></Navigation>
     </header>
   );
 }
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,19 +1,48 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
+
+import logoutIcon from '../../images/icons/button-logout-icon.svg';
+import logoutIconDark from '../../images/icons/button-logout-icon-dark.svg';
 
 function Navigation() {
+  const testUserName = 'Elise';
   return (
     <nav className='navigation'>
-      <ul className='navigation__link-list'>
-        <li className='navigation__link-list-item navigation__link-list-item_active'>
-          <Link to='/' className='navigation__link'>
+      <Link to='/' className='navigation__title'>
+        NewsExplorer
+      </Link>
+      <ul className='navigation__list'>
+        <li>
+          <NavLink
+            to='/'
+            className='navigation__link'
+            activeClassName='navigation__link_active'
+          >
             Home
-          </Link>
+          </NavLink>
         </li>
-        <li className='navigation__link-list-item navigation__link-list-item_articles navigation__link-list-item_active'>
-          <Link to='/saved-news' className='navigation__link'>
+        <li>
+          <Link
+            to='/saved-news'
+            className='navigation__link navigation__link_active navigation__link_articles'
+          >
             Saved articles
           </Link>
+        </li>
+        {/*         <li>
+          <button className='navigation__button navigation__button_signin'>
+            Sign In
+          </button>
+        </li> */}
+        <li>
+          <button className='navigation__button navigation__button_logout'>
+            <span className='navigation__username'>{testUserName}</span>
+            <img
+              className='navigation__logout-icon'
+              alt='logout'
+              src={logoutIcon}
+            />
+          </button>
         </li>
       </ul>
     </nav>
